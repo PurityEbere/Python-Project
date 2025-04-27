a22caa128d157e4e732dfad8cbb338f2e3205a3b
Purityebere <Udeheberepurity@gmail.com>
2025-04-27 12:11:42 +0100

    made few changes and fixes

--- number_guessing.py
+++ number_guessing.py
@@ -1,14 +1,16 @@
+
 import random
 
 min_value = int(input('Enter a minimum value: '))
 max_value = int(input('Enter a maximum value: '))
+max_attempts = int(input('Enter the maximum number of attempts allowed: '))
 guess_count = 0
 number_to_guess = random.randint(min_value, max_value)
 
-while True:
+while guess_count < max_attempts:
     try:
         guess = int(input(f'Guess the number between {min_value} and {max_value}: '))
-        guess_count += 1  
+        guess_count += 1 
         if guess < number_to_guess:
             print('Too low! Try again.')
         elif guess > number_to_guess:
@@ -18,3 +20,8 @@ while True:
             break  
     except ValueError:
         print("Invalid input. Please enter a valid number.")
+else:
+    if guess_count > 1:
+        print(f'Game over my gee, you have used all {max_attempts} attempts. The correct number was {number_to_guess}.')
+    else:
+       print(f"Game over! You've used all attempts")
